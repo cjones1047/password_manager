b3ae70b620840557be0607a5ce9d5c82b73ab758
cjones1047 <cjmj1047@gmail.com>
2022-12-09 06:30:40 -0600

    Updated fonts to be default

--- main.py
+++ main.py
@@ -13,25 +13,25 @@ window = Tk()
 window.title("Password Manager")
 window.config(padx=50, pady=50)
 
-canvas = Canvas(width=200, height=200, highlightthickness=0)
+canvas = Canvas(width=200, height=200)
 padlock_img = PhotoImage(file='logo.png')
 canvas.create_image(100, 100, image=padlock_img)
-canvas.grid(column=0, row=0, columnspan=3)
+canvas.grid(column=1, row=0)
 
-website_label = Label(text="Website:", font=FONT_TUPLE)
+website_label = Label(text="Website:")
 website_label.grid(column=0, row=1)
 
 website_entry = Entry(width=35)
 website_entry.focus()
 website_entry.grid(column=1, row=1, columnspan=2)
 
-user_label = Label(text="Email/Username:", font=FONT_TUPLE)
+user_label = Label(text="Email/Username:")
 user_label.grid(column=0, row=2)
 
 user_entry = Entry(width=35)
 user_entry.grid(column=1, row=2, columnspan=2)
 
-pw_label = Label(text="Password:", font=FONT_TUPLE)
+pw_label = Label(text="Password:")
 pw_label.grid(column=0, row=3)
 
 pw_entry = Entry(width=21)
