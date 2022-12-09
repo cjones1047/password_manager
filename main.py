6017a3aff89e5b33b2f87188a0da47f2f28b6cbe
cjones1047 <cjmj1047@gmail.com>
2022-12-09 06:20:18 -0600

    Added entry fields for website, username and password with labels; added buttons for generating random passwords and adding password generated or entered

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from tkinter import *
 
+FONT_TUPLE = ("Arial", 17, "normal")
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
@@ -9,11 +11,36 @@ from tkinter import *
 
 window = Tk()
 window.title("Password Manager")
-window.config(padx=20, pady=20)
+window.config(padx=50, pady=50)
 
 canvas = Canvas(width=200, height=200, highlightthickness=0)
 padlock_img = PhotoImage(file='logo.png')
 canvas.create_image(100, 100, image=padlock_img)
-canvas.grid(column=1, row=1)
+canvas.grid(column=0, row=0, columnspan=3)
+
+website_label = Label(text="Website:", font=FONT_TUPLE)
+website_label.grid(column=0, row=1)
+
+website_entry = Entry(width=35)
+website_entry.focus()
+website_entry.grid(column=1, row=1, columnspan=2)
+
+user_label = Label(text="Email/Username:", font=FONT_TUPLE)
+user_label.grid(column=0, row=2)
+
+user_entry = Entry(width=35)
+user_entry.grid(column=1, row=2, columnspan=2)
+
+pw_label = Label(text="Password:", font=FONT_TUPLE)
+pw_label.grid(column=0, row=3)
+
+pw_entry = Entry(width=21)
+pw_entry.grid(column=1, row=3)
+
+gen_pw_button = Button(text="Generate Password")
+gen_pw_button.grid(column=2, row=3)
+
+add_pw_button = Button(text="Add", width=36)
+add_pw_button.grid(column=1, row=4, columnspan=2)
 
 window.mainloop()
