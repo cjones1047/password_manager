9ee17fe0131861e2103cef582a06a4ba7d45469e
cjones1047 <cjmj1047@gmail.com>
2022-12-12 06:17:30 -0600

    Added default user_entry text, updated sticky property of all GUI elements to more specifically format columns

--- main.py
+++ main.py
@@ -16,31 +16,32 @@ window.config(padx=50, pady=50)
 canvas = Canvas(width=200, height=200)
 padlock_img = PhotoImage(file='logo.png')
 canvas.create_image(100, 100, image=padlock_img)
-canvas.grid(column=1, row=0)
+canvas.grid(column=0, row=0, columnspan=3)
 
 website_label = Label(text="Website:")
-website_label.grid(column=0, row=1)
+website_label.grid(column=0, row=1, sticky='E')
 
 website_entry = Entry(width=35)
 website_entry.focus()
-website_entry.grid(column=1, row=1, columnspan=2)
+website_entry.grid(column=1, row=1, columnspan=2, sticky='W')
 
 user_label = Label(text="Email/Username:")
-user_label.grid(column=0, row=2)
+user_label.grid(column=0, row=2, sticky='E')
 
 user_entry = Entry(width=35)
-user_entry.grid(column=1, row=2, columnspan=2)
+user_entry.insert(0, "cjmj1047@gmail.com")
+user_entry.grid(column=1, row=2, columnspan=2, sticky='W')
 
 pw_label = Label(text="Password:")
-pw_label.grid(column=0, row=3)
+pw_label.grid(column=0, row=3, sticky='E')
 
 pw_entry = Entry(width=21)
-pw_entry.grid(column=1, row=3)
+pw_entry.grid(column=1, row=3, sticky='W')
 
 gen_pw_button = Button(text="Generate Password")
 gen_pw_button.grid(column=2, row=3)
 
 add_pw_button = Button(text="Add", width=36)
-add_pw_button.grid(column=1, row=4, columnspan=2)
+add_pw_button.grid(column=0, row=4, columnspan=3)
 
 window.mainloop()
